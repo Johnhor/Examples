cb8b41ed4dc68e1e3a03aa1af05e7e3e53c62c11
vanea hortolomei <hortolomei1989@gmail.com>
2019-05-11 20:38:45 +0300

    add button

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+// let close = document.querySelector('spans');
+// close.addEventListener('click', function(){
+// 	console.log('hello')
+// })
+
+let btn = document.querySelector('button');
+btn.addEventListener('click', function(){
+	 console.log('hi');
+})
