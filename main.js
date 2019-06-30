8966c7ffa9cc601d5bb3b21bf3aaa55e0144a006
vanea hortolomei <hortolomei1989@gmail.com>
2019-06-30 13:46:26 +0300

    change js file

---
+++ main.js
@@ -0,0 +1,18 @@
+
+const button = document.querySelectorAll('.offer-card__header');
+
+for(let i = 0; i < button.length; i++){
+	button[i].addEventListener('click', function(){
+		
+	this.classList.toggle("active");
+    let content = this.nextElementSibling;
+		 if(content.style.maxHeight){
+   	 content.style.maxHeight = null;
+   	 
+   }
+   else {
+   	 content.style.maxHeight = content.scrollHeight + "px";
+   	 
+   }
+	})
+}
