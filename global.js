b8363cc4a4c0f1c696be82848f70971b4b73ac2f
vanea hortolomei <hortolomei1989@gmail.com>
2019-06-30 11:59:51 +0300

    update feed

--- global.js
+++ global.js
@@ -1,55 +1,55 @@
-// const colors = ['red','blue','orange','yellow','green'];
-// const btn = document.querySelector('.button').addEventListener('click', changeColor = () => {
-// 	let random = Math.floor(Math.random() * colors.length);
-// 	document.body.style.backgroundColor = colors[random];
-// });
+const colors = ['red','blue','orange','yellow','green'];
+const btn = document.querySelector('.button').addEventListener('click', changeColor = () => {
+	let random = Math.floor(Math.random() * colors.length);
+	document.body.style.backgroundColor = colors[random];
+});
 
 
-// const hexColors = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D',
-// 'E','F'];
+const hexColors = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D',
+'E','F'];
 
-// const nextBtn = document.querySelector('.hex-button');
-// const place = document.querySelector('.color-hex');
+const nextBtn = document.querySelector('.hex-button');
+const place = document.querySelector('.color-hex');
 
-// nextBtn.addEventListener('click',changeHex);
-// function changeHex() { 
-//   let hex = '#';
-//   for(let i = 0; i < 6; i++){
-//   	 let randomHex = Math.floor(Math.random()*hexColors.length);
-//   	 hex += hexColors[randomHex];
-//   }
-//   document.body.style.backgroundColor = hex;
-//   place.innerHTML = hex;
-// }
+nextBtn.addEventListener('click',changeHex);
+function changeHex() { 
+  let hex = '#';
+  for(let i = 0; i < 6; i++){
+  	 let randomHex = Math.floor(Math.random()*hexColors.length);
+  	 hex += hexColors[randomHex];
+  }
+  document.body.style.backgroundColor = hex;
+  place.innerHTML = hex;
+}
 
-// const randomPlace = document.querySelector('.random-place');
-// const arrays = [1,2,3,4,5,6,7,8,9]
-// function random(){
-// 	let b = 'number is: ';
-//   for(let i = 0; i < 5; i++ ){
-//         let a = Math.floor(Math.random()*arrays.length);
-//         b += arrays[a];
-//         console.log(b);
-//   }
-//   randomPlace.innerHTML = b;
-// }
-// random();
-
-
-// const ar = [2,3,4];
-// let total = 0;
-// for(let d of ar){
-// 	 d += 1;
-// 	 total += d;
-// 	 console.log(d);
-// };
+const randomPlace = document.querySelector('.random-place');
+const arrays = [1,2,3,4,5,6,7,8,9]
+function random(){
+	let b = 'number is: ';
+  for(let i = 0; i < 5; i++ ){
+        let a = Math.floor(Math.random()*arrays.length);
+        b += arrays[a];
+        console.log(b);
+  }
+  randomPlace.innerHTML = b;
+}
+random();
 
 
+const ar = [2,3,4];
+let total = 0;
+for(let d of ar){
+	 d += 1;
+	 total += d;
+	 console.log(d);
+};
 
-const btn = document.querySelectorAll('.offer-card__header');
 
-for(let i = 0; i < btn.length; i++){
-	btn[i].addEventListener('click', function(){
+
+const button = document.querySelectorAll('.offer-card__header');
+
+for(let i = 0; i < button.length; i++){
+	button[i].addEventListener('click', function(){
 		
 	this.classList.toggle("active");
     let content = this.nextElementSibling;
